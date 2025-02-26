4decae66a4459a65697e4f7b9300280a7aaad4a6
Elowen Luo <74773924+LYingYuan@users.noreply.github.com>
2025-02-26 09:11:35 +0800

    revert(server): remove configurable timeout setting Refs: 662e733

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "http://localhost";
-const TIMEOUT = process.env.TIMEOUT || 300000;
 
 // Start server
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(
     `Image download endpoint: ${HOST}:${PORT}/downloadImage/:imageName`
   );
   console.log(`Article processing endpoint:  ${HOST}:${PORT}/process`);
 });
-
-server.timeout = Number(TIMEOUT);
 
 // Schedule cleanup tasks
 const cleanupTools = new CleanupTools(1);
